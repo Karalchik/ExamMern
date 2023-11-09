a6609d279e03a0629fc7aea5e52d069eccdabdef
Karalchik <ddtera636@gmail.com>
2023-11-09 18:43:44 +0100

    frontend/navbar-rebuild

--- mern-boat-rent/src/components/navbar.component.js
+++ mern-boat-rent/src/components/navbar.component.js
@@ -6,18 +6,54 @@ export default class Navbar extends Component {
   render() {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">ExcerTracker</Link>
+        <div name="block-icon"></div>
+        <Link to="/" className="navbar-brand">BLUE SHELL</Link>
         <div className="collpase navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
+          <Link to="/" className="nav-link">RENT BOAT</Link>
+          <div>
+          <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">FIND BOAT</Link>
           </li>
-          {/* <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
+          </ul>
+          </div>
           </li>
           <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li> */}
+          <Link to="/" className="nav-link">ABOUT</Link>
+          <div>
+          <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">CONTACT</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">ABOUT US</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">FAQ</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">INFORMATION</Link>
+          </li>
+          </ul>
+          </div>
+          </li>
+          <li className="navbar-item">
+          <Link to="/" className="nav-link">PROFILE</Link>
+          </li>
+          <li className="navbar-item">
+          <div>
+          <ul className="navbar-nav mr-auto">
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">CHOOSE LANGUAGE</Link>
+          </li>
+          <li className="navbar-item">
+            <Link to="/" className="nav-link">NUMBER</Link>
+          </li>
+          </ul>
+          </div>
+          </li>
         </ul>
         </div>
       </nav>
